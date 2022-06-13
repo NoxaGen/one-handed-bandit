d28646716647aa4700a3a36fc814cc0cd1cd90af
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-13 17:07:14 +0200

    download all requied elements from html and create 2 methods

--- Game.js
+++ Game.js
@@ -1,1 +1,25 @@
+class Game {
+    constructor() {
+        this.stats = new Statistics();
+        this.wallet = new Wallet(200);
+        document.getElementById('spin').addEventListener('click', this.startGame)
+        //spans 
+        this.walletSaldo = document.querySelector('[data-summary="user-cash"]');
+        this.lostCash = document.querySelector('[data-summary="lost-cash"]');
+        this.numberOfSpins = document.querySelector('[data-summary="spins"]');
+        this.numberOfWins = document.querySelector('[data-summary="wins"]');
+        this.numberOfLoses = document.querySelector('[data-summary="losses"]');
 
+        this.render()
+    }
+
+    //method for refreshing all information 
+    render() {
+        console.log('gramy ziomo')
+    }
+
+    startGame() {
+
+    }
+
+}
