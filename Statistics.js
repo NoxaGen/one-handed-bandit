8d3fa04e1a2ffb3c2a5c0c5234667b7ba64d97c4
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-13 15:10:18 +0200

    adjust and refactor my old class Statistics.js

--- Statistics.js
+++ Statistics.js
@@ -1,26 +1,28 @@
 class Statistics {
- constructor() {
-  this.gameResults = [{ win: true, bid: 2 }, { win: true, bid: 12 }, { win: false, bid: -10 }];
+    constructor() {
+        //this array is created for store game history from method below
+        this.gameResults = []
+    }
 
- }
+    //method wich requies 2 args
+    //example: user wins and won 10$, so we need to put (true, 10)
+    //example2: user set bid 
+    addGameToStatistics(win, bid) {
+        let gameResult = {
+            win: win,
+            bid: bid
+        }
+        console.log(gameResult);
+        this.gameResults.push(gameResult)
+    }
 
- addGameToStatistics(win, bid) {
-  let gameResult = {
-   win,
-   bid
-  }
-  console.log(gameResult);
-  this.gameResults.push(gameResult)
- }
-
- showGameStatistics() {
-  let games = this.gameResults.length;
-  let wins = this.gameResults.filter(result => result.win).length;
-  let losses = this.gameResults.filter(result => !result.win).length
-  console.log(games, wins, losses);
-  return [games, wins, losses]
- }
-
+    showGameStatistics() {
+        let games = this.gameResults.length;
+        let wins = this.gameResults.filter(result => result.win).length;
+        let losses = this.gameResults.filter(result => !result.win).length
+        console.log(games, wins, losses);
+        return [games, wins, losses]
+    }
 }
 
 // const stats = new Statistics()
