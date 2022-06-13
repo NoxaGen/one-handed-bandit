bb1c2db8b3fe145e49c1722dca1b3a27db96f47b
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-13 17:39:38 +0200

    upgrade render method

--- Game.js
+++ Game.js
@@ -5,19 +5,23 @@ class Game {
         document.getElementById('spin').addEventListener('click', this.startGame)
         //spans 
         this.inputBid = document.querySelector('.panel .text input');
-        this.walletSaldo = document.querySelector('[data-summary="user-cash"]');
+        this.walletSaldo = document.querySelector('[data-summary="user-cash"]'); //money
         this.lostCash = document.querySelector('[data-summary="lost-cash"]');
-        this.numberOfSpins = document.querySelector('[data-summary="spins"]');
-        this.numberOfWins = document.querySelector('[data-summary="wins"]');
-        this.numberOfLoses = document.querySelector('[data-summary="losses"]');
+        this.numberOfSpins = document.querySelector('[data-summary="spins"]'); //0 stats
+        this.numberOfWins = document.querySelector('[data-summary="wins"]'); //1 stats
+        this.numberOfLoses = document.querySelector('[data-summary="losses"]'); //2 stats
         this.fruitsInMachine = [...document.querySelectorAll('[data-option="fruit"]')];
 
         this.render()
     }
 
     //method for refreshing all information 
-    render() {
-        console.log('gramy ziomo')
+    render(money = this.wallet.getWalletValue(), stats = [0, 0, 0]) {
+        this.walletSaldo.textContent = money;
+        this.numberOfSpins.textContent = stats[0];
+        this.numberOfWins.textContent = stats[1];
+        this.numberOfLoses.textContent = stats[2];
+
     }
 
     startGame() {
