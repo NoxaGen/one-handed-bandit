c16c90689a952f4df5323f82667edaaecaab78cd
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-13 17:11:18 +0200

    download all fruits in the machine and put them in arr

--- Game.js
+++ Game.js
@@ -4,11 +4,13 @@ class Game {
         this.wallet = new Wallet(200);
         document.getElementById('spin').addEventListener('click', this.startGame)
         //spans 
+        this.inputBid = document.querySelector('.panel .text input');
         this.walletSaldo = document.querySelector('[data-summary="user-cash"]');
         this.lostCash = document.querySelector('[data-summary="lost-cash"]');
         this.numberOfSpins = document.querySelector('[data-summary="spins"]');
         this.numberOfWins = document.querySelector('[data-summary="wins"]');
         this.numberOfLoses = document.querySelector('[data-summary="losses"]');
+        this.fruitsInMachine = [...document.querySelectorAll('[data-option="fruit"]')];
 
         this.render()
     }
