03b5279268b7b926132a6e7d0af34b4a02ea47ea
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-14 12:38:13 +0200

    do little refactor and fix comments

--- Game.js
+++ Game.js
@@ -3,23 +3,20 @@ class Game {
         this.stats = new Statistics();
         this.wallet = new Wallet(200);
         document.getElementById('spin').addEventListener('click', this.startGame)
-        //spans 
         this.inputBid = document.querySelector('.panel .text input');
+        //spans 
         this.walletSaldo = document.querySelector('[data-summary="user-cash"]'); //money
         this.lostCash = document.querySelector('[data-summary="lost-cash"]');
         this.numberOfSpins = document.querySelector('[data-summary="spins"]'); //0 stats
         this.numberOfWins = document.querySelector('[data-summary="wins"]'); //1 stats
         this.numberOfLoses = document.querySelector('[data-summary="losses"]'); //2 stats
+        //fruit divs
         this.fruitsInMachine = [...document.querySelectorAll('[data-option="fruit"]')];
-
+        //render method from below
         this.render()
     }
 
-    //method for refreshing all information 
-    //te właściwości domyślne są przekazane tylko po to, żeby przy wczytaniu
-    //metody render, mieć jakieś początkowe parametry ONE WSZYSTKIE
-    //zostaną nadpisane :)
-
+    //all this params are startup params - they will be raplaced by another method
     render(render = ['url(./images/dolar_start_sign.png)', 'url(./images/dolar_start_sign.png)', 'url(./images/dolar_start_sign.png)'],
         money = this.wallet.getWalletValue(), stats = [0, 0, 0], lostMoney = 0, result = "", bid = 0, wonMoney = 0) {
         this.walletSaldo.textContent = money;
