4f13f5399e394c6a0403f9236d9016c6d09c99e2
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-14 13:40:26 +0200

    add some comments for better understand and upgrade method

--- Game.js
+++ Game.js
@@ -2,8 +2,10 @@ class Game {
     constructor() {
         this.stats = new Statistics();
         this.wallet = new Wallet(200);
-        document.getElementById('spin').addEventListener('click', this.startGame)
-        this.inputBid = document.querySelector('.panel .text input');
+        //addEventListener zrywa połączenie this'a, dlatego trzeba je 'związać' bindem
+        //w ten sposób jest jakby utworzona nowa metoda która przechowuje poprawną referencje do thisa
+        document.getElementById('spin').addEventListener('click', this.startGame.bind(this));
+        this.inputBid = document.getElementById('bid');
         //spans 
         this.walletSaldo = document.querySelector('[data-summary="user-cash"]'); //money
         this.lostCash = document.querySelector('[data-summary="lost-cash"]');
@@ -35,7 +37,21 @@ class Game {
 
     }
 
+    //why this doesn't work but when i called instance game its working?
+    //wiązanie zrywa tez addEventListener podobnie jak foreach?
+    // rozwiązanie to nie game, tylko użycie ZWIĄZANIA - BIND'a
     startGame() {
+        //this statment checks is value is minimum 1, mathfloor also convert string into number in hidden process
+        if (this.inputBid.value < 1) return alert('Minimalna wartość aby zagrać nie może być mniejsza niż 1$')
+        //this var store passed bid in the inpute so we can use it in this method 
+        const bid = Math.floor(this.inputBid.value);
+
+        //this statment calling method from Wallet class that check user have enough coins to play
+        if (!this.wallet.checkCanPlay(bid)) return alert("Masz za mało środków aby zagrać!")
+
+        //call of the method that allows us 
+        this.wallet.changeWalletValue(bid, '-');
+
 
     }
 
